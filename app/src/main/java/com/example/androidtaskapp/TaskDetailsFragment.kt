d7d8950c243e97fb89bad0d84e79c7b761d27b65
Stanley Reynara <stanleyreynara@gmail.com>
2024-05-18 14:27:31 +0700

    Most FrontEnd functionality added

---
+++ app/src/main/java/com/example/androidtaskapp/TaskDetailsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.androidtaskapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TaskDetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TaskDetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_details, container, false)
+    }
+
+}
